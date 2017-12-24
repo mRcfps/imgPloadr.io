e60c3e9d0bdf5967aba9500012a3d7f2cf981889
mRcfps <1402491442@qq.com>
2017-12-24 11:34:04 +0800

    [ADD] web app entrypoint

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var path = require('path');
+
+// var config = require('./server/configure');
+
+app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+
+// app = config(app);
+
+app.get('/', function (req, res) {
+    res.send('Hello World');
+});
+
+app.listen(app.get('port'), function () {
+    console.log('Server up: http://localhost:' + app.get('port'));
+});
