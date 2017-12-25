2ba01293b4185039699e780f3c2a1e76ab1c4ed2
mRcfps <1402491442@qq.com>
2017-12-25 21:37:44 +0800

    [ADD] mongodb connection

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var mongoose = require('mongoose');
 var path = require('path');
 
 var config = require('./server/configure');
@@ -9,6 +10,10 @@ app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 
 app = config(app);
+mongoose.connect('mongodb://localhost/imgPloadr');
+mongoose.connection.on('open', function () {
+  console.log('Mongoose connected.');
+});
 
 app.listen(app.get('port'), function () {
     console.log('Server up: http://localhost:' + app.get('port'));
