3727b28fc7bb84c5984227e40e4f0342658f7470
mRcfps <1402491442@qq.com>
2017-12-26 17:54:54 +0800

    [ADD] async logic for helpers

--- helpers/sidebar.js
+++ helpers/sidebar.js
@@ -1,13 +1,25 @@
+var async = require('async');
 var Stats = require('./stats');
 var Images = require('./images');
 var Comments = require('./comments');
 
 module.exports = function (viewModel, callback) {
-  viewModel.sidebar = {
-    stats: Stats(),
-    popular: Images.popular(),
-    comments: Comments.newest()
-  };
-
-  callback(viewModel);
+  async.parallel([
+    function (next) {
+      Stats(next);
+    },
+    function (next) {
+      Images.popular(next);
+    },
+    function (next) {
+      Comments.newest(next);
+    }
+  ], function (err, results) {
+    viewModel.sidebar = {
+      stats: results[0],
+      popular: results[1],
+      comments: results[2]
+    };
+    callback(viewModel);
+  });
 };
--- helpers/stats.js
+++ helpers/stats.js
@@ -1,10 +1,58 @@
-module.exports = function () {
-  var stats = {
-    images: 0,
-    comments: 0,
-    views: 0,
-    likes: 0
-  };
+var async = require('async');
+var models = require('../models');
 
-  return stats;
+module.exports = function (callback) {
+  async.parallel([
+    function (next) {
+      // find the total number of images
+      models.Image.count({}, next);
+    },
+    function (next) {
+      // find the total number of comments
+      models.Comment.count({}, next);
+    },
+    function (next) {
+      // sum up views of all images
+      models.Image.aggregate(
+        {
+          $group: {
+            _id: '1',
+            viewsTotal: { $sum: '$views' }
+          }
+        },
+        function (err, result) {
+          var viewsTotal = 0;
+          if (result.length > 0) {
+            viewsTotal += result[0].viewsTotal;
+          }
+          next(null, viewsTotal);
+        }
+      );
+    },
+    function (next) {
+      // sum up likes of all images
+      models.Image.aggregate(
+        {
+          $group: {
+            _id: '1',
+            likesTotal: { $sum: '$likes' }
+          }
+        },
+        function (err, result) {
+          var likesTotal = 0;
+          if (result.length > 0) {
+            likesTotal += result[0].likesTotal;
+          }
+          next(null, likesTotal);
+        }
+      );
+    }
+  ], function (err, results) {
+    callback(null, {
+      image: results[0],
+      comments: results[1],
+      views: results[2],
+      likes: results[3],
+    });
+  });
 };
